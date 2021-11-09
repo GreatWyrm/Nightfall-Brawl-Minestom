54be17e2fd48041847f942bcbfc4d7005dae76fc
GreatWyrm <alecmusante@gmail.com>
2021-11-08 20:01:46 -0500

    Updated dependencies, added extensions to gitignore, and added ability to launch server from bootstrap.

---
+++ src/test/java/me/arcanewarrior/test/Bootstrapper.java
@@ -0,0 +1,9 @@
+package me.arcanewarrior.test;
+
+import net.minestom.server.Bootstrap;
+
+public class Bootstrapper {
+    public static void main(String[] args) {
+        Bootstrap.bootstrap("me.arcanewarrior.com.StartServer", args);
+    }
+}
